bcf0faf643c4aefdefd66df54f84483a718a6a88
Tilashma Neupane <tilashmaneupane@gmail.com>
2026-04-21 17:59:48 +0545

    initial study plan release

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 1. Add this import
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/study-plan/', // Important for GitHub subfolders!
+  plugins: [
+    react(), 
+    tailwindcss() // 2. Add this to the list
+  ],
+  base: '/study-plan/', 
 })
-
